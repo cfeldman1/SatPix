3f777b7fa5919f6fd8d001eb8144279ac32d9989
ColeFeldman <ColeFeldman@coles-air-3.internal.hws.org>
2015-10-13 09:11:24 -0700

    Started the reading in of the file with scanner. 1st commit.

---
+++ SatPix/src/SatPix.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class SatPix {
+
+	public static void main(String[] args) throws IOException
+	{
+		boolean[][] booleanArr = fileToBoolArray("satpix.in");
+		int sizeOfLargestPasture;
+		int col;
+		int row; 
+		try{
+		Scanner scan = new Scanner(new File("satpix.in"));
+		
+		col = scan.nextInt();
+		row = scan.nextInt();
+		while (scan.hasNextLine())
+		{
+			
+		}
+			scan.close();
+		/* YOUR CODE GOES HERE */
+		
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("satpix.out")));
+		out.println(sizeOfLargestPasture);
+		out.close();
+		}
+	
+	private static boolean[][] fileToBoolArray(String fileName) throws FileNotFoundException, IOException
+	{
+		//This helper method converts the input file into a 2D array of booleans
+	}
+
+	private static int recursivelyMeasureAndMarkPasture(int row, int col, boolean[][] arr)
+	{
+		//This recursive method employs the flood-fill algorithm to
+		//count the size of a single pasture and "mark" it so it is not double-counted
+		return 0;
+	}
+	
+}
